99f29c8f7dfccfe3a11b9951e76bf5280ae4535d
Marius Feldmann <feldmann.marius@gmail.com>
2015-10-16 17:42:25 +0200

    removed old files

--- add.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-date=$(date +%s)
-while read -r replace with; do
-  plist+="{on=1;replace=\"$replace\";with=\"$with\";},"
-  sql+="INSERT INTO 'ZUSERDICTIONARYENTRY' VALUES($((++i)),1,1,0,0,0,0,$date,NULL,NULL,NULL,NULL,NULL,\"$with\",\"$replace\",NULL);"
-done < <(sed 's/\\/\\\\/g;s/"/\\"/g' emojis.txt)
-sqlite3 ~/Library/Dictionaries/CoreDataUbiquitySupport/$USER~*/UserDictionary/local/store/UserDictionary.db "delete from ZUSERDICTIONARYENTRY;$sql"
-defaults write -g NSUserDictionaryReplacementItems "(${plist%?})"
